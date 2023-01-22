c776df4f0ab696449b75503f367323ecbc41ca87
JaViKo <javikogutierrez64@gmail.com>
2023-01-21 22:14:25 -0500

    add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
+
+const hbs = require('hbs');
+
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 // Server static content
 app.use( express.static('./public') );
-
-app.get('/hello-world', function (req, res) {
-  res.send('Hello World');
+app.get('/', function (req, res) {
+  res.render( 'home', {
+    name: 'Javiko',
+    title: 'Node course'
+  } );
 });
+app.get('/generic', function (req, res) {
+  res.render( 'generic', {
+    name: 'Javiko',
+    title: 'Node course'
+  } );
+});
+app.get('/elements', function (req, res) {
+  res.render( 'elements', {
+    name: 'Javiko',
+    title: 'Node course'
+  } );
+});
+// app.get('/generic', function (req, res) {
+//   res.sendFile(__dirname + '/public/generic.html');
+// });
+// app.get('/elements', function (req, res) {
+//   res.sendFile(__dirname + '/public/elements.html');
+// });
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/404.html');
 });
